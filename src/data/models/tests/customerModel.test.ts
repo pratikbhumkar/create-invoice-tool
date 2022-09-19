5a746d639d843961980d4a743b279753be443f0e
pratikbhumkar <prateekbhumkar@gmail.com>
2022-09-19 13:17:21 +1000

    add mongo tests

---
+++ src/data/models/tests/customerModel.test.ts
@@ -0,0 +1,105 @@
+import { CustomerInterface } from '../../../dataInterface/customerInterface'
+import { CustomerModel } from '../customerModel'
+
+describe('testing mongoose models', () => {
+  test('should create a mongoose model for customer', () => {
+    const customer: CustomerInterface = {
+      name: 'test',
+      address: 'address',
+      phone_number: 'phone_number',
+      disabled: false,
+      rate: 100,
+      subject: 'node',
+      email: 'node@node.com',
+      abn: 123345
+    }
+    const customerInstance = new CustomerModel(customer)
+    expect(customerInstance.name).toEqual(customer.name)
+    expect(customerInstance.address).toEqual(customer.address)
+    expect(customerInstance.phone_number).toEqual(customer.phone_number)
+    expect(customerInstance.disabled).toEqual(customer.disabled)
+    expect(customerInstance.rate).toEqual(customer.rate)
+    expect(customerInstance.subject).toEqual(customer.subject)
+    expect(customerInstance.email).toEqual(customer.email)
+    expect(customerInstance.abn).toEqual(customer.abn)
+  })
+
+  test('should not create an object if name is missing', () => {
+    const customer = {
+      address: 'address',
+      phone_number: 'phone_number',
+      disabled: false,
+      rate: 100,
+      subject: 'node',
+      email: 'node@node.com',
+      abn: 123345
+    }
+    const customerInstance = new CustomerModel(customer)
+    expect(customerInstance.validateSync).toThrowError()
+  })
+  test('should not create an object if phone_number is missing', () => {
+    const customer = {
+      name: 'test',
+      address: 'address',
+      disabled: false,
+      rate: 100,
+      subject: 'node',
+      email: 'node@node.com',
+      abn: 123345
+    }
+    const customerInstance = new CustomerModel(customer)
+    expect(customerInstance.validateSync).toThrowError()
+  })
+  test('should not create an object if disabled is missing', () => {
+    const customer = {
+      name: 'test',
+      address: 'address',
+      phone_number: 'phone_number',
+      rate: 100,
+      subject: 'node',
+      email: 'node@node.com',
+      abn: 123345
+    }
+    const customerInstance = new CustomerModel(customer)
+    expect(customerInstance.validateSync).toThrowError()
+  })
+  test('should not create an object if rate is missing', () => {
+    const customer = {
+      name: 'test',
+      address: 'address',
+      phone_number: 'phone_number',
+      subject: 'node',
+      email: 'node@node.com',
+      abn: 123345,
+      disabled: true
+    }
+    const customerInstance = new CustomerModel(customer)
+    expect(customerInstance.validateSync).toThrowError()
+  })
+  test('should not create an object if subject is missing', () => {
+    const customer = {
+      name: 'test',
+      address: 'address',
+      phone_number: 'phone_number',
+      email: 'node@node.com',
+      abn: 123345,
+      disabled: true,
+      rate: 100
+    }
+    const customerInstance = new CustomerModel(customer)
+    expect(customerInstance.validateSync).toThrowError()
+  })
+  test('should not create an object if email is missing', () => {
+    const customer = {
+      name: 'test',
+      address: 'address',
+      phone_number: 'phone_number',
+      abn: 123345,
+      disabled: true,
+      rate: 100,
+      subject: 'sub'
+    }
+    const customerInstance = new CustomerModel(customer)
+    expect(customerInstance.validateSync).toThrowError()
+  })
+})
---
+++ src/data/models/tests/incoiceModel.test.ts
@@ -0,0 +1,97 @@
+import { InvoiceInterface } from '../../../dataInterface/invoiceInterface'
+import { InvoiceModel } from '../invoiceModel'
+
+describe('testing mongoose models', () => {
+  test('should create a mongoose model for invoice', () => {
+    const invoice: InvoiceInterface = {
+      invoiceNumber: 1234,
+      customerName: 'customerName',
+      billeditems: [{ item: 'item', rate: 100 }],
+      price: 100,
+      description: 'description',
+      dateOfIssue: new Date(),
+      disabled: true
+    }
+    const invoiceInstance = new InvoiceModel(invoice)
+    expect(invoiceInstance.invoiceNumber).toEqual(invoice.invoiceNumber)
+    expect(invoiceInstance.customerName).toEqual(invoice.customerName)
+    expect(invoiceInstance.billeditems[0].item).toEqual(invoice.billeditems[0].item)
+    expect(invoiceInstance.billeditems[0].rate).toEqual(invoice.billeditems[0].rate)
+    expect(invoiceInstance.price).toEqual(invoice.price)
+    expect(invoiceInstance.description).toEqual(invoice.description)
+    expect(invoiceInstance.dateOfIssue).toEqual(invoice.dateOfIssue)
+    expect(invoiceInstance.disabled).toEqual(invoice.disabled)
+  })
+  test('should throw an error for missing invoice Number', () => {
+    const invoice = {
+      customerName: 'customerName',
+      billeditems: [{ item: 'item', rate: 100 }],
+      price: 100,
+      description: 'description',
+      dateOfIssue: new Date(),
+      disabled: true
+    }
+    const invoiceInstance = new InvoiceModel(invoice)
+    expect(invoiceInstance.validateSync).toThrowError()
+  })
+  test('should throw an error for missing customer name', () => {
+    const invoice = {
+      invoiceNumber: 1234,
+      billeditems: [{ item: 'item', rate: 100 }],
+      price: 100,
+      description: 'description',
+      dateOfIssue: new Date(),
+      disabled: true
+    }
+    const invoiceInstance = new InvoiceModel(invoice)
+    expect(invoiceInstance.validateSync).toThrowError()
+  })
+  test('should throw an error for missing billedItems', () => {
+    const invoice = {
+      invoiceNumber: 1234,
+      customerName: 'customerName',
+      price: 100,
+      description: 'description',
+      dateOfIssue: new Date(),
+      disabled: true
+    }
+    const invoiceInstance = new InvoiceModel(invoice)
+    expect(invoiceInstance.validateSync).toThrowError()
+  })
+  test('should throw an error for missing price', () => {
+    const invoice = {
+      invoiceNumber: 1234,
+      customerName: 'customerName',
+      billeditems: [{ item: 'item', rate: 100 }],
+      description: 'description',
+      dateOfIssue: new Date(),
+      disabled: true
+    }
+    const invoiceInstance = new InvoiceModel(invoice)
+    expect(invoiceInstance.validateSync).toThrowError()
+  })
+  test('should throw an error for missing dateOfIssue', () => {
+    const invoice = {
+      invoiceNumber: 1234,
+      customerName: 'customerName',
+      billeditems: [{ item: 'item', rate: 100 }],
+      price: 100,
+      description: 'description',
+      disabled: true
+    }
+    const invoiceInstance = new InvoiceModel(invoice)
+    expect(invoiceInstance.validateSync).toThrowError()
+  })
+  test('should throw an error for missing disabled', () => {
+    const invoice = {
+      invoiceNumber: 1234,
+      customerName: 'customerName',
+      billeditems: [{ item: 'item', rate: 100 }],
+      price: 100,
+      description: 'description',
+      dateOfIssue: new Date()
+    }
+    const invoiceInstance = new InvoiceModel(invoice)
+    expect(invoiceInstance.validateSync).toThrowError()
+  })
+})
