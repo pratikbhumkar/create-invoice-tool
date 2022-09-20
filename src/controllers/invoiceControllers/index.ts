282296f2b9289bb6b313c184f729cbe18c7a4028
pratikbhumkar <prateekbhumkar@gmail.com>
2022-09-20 10:55:42 +1000

    add controller calls and tests

---
+++ src/controllers/invoiceControllers/index.ts
@@ -0,0 +1,19 @@
+import { InvoiceInterface } from '../../dataInterface/invoiceInterface'
+import { createInvoiceHandler } from '../../services/invoiceServices/createInvoiceHandler'
+import { disableInvoiceHandler } from '../../services/invoiceServices/disableInvoiceHandler'
+import { enableInvoiceHandler } from '../../services/invoiceServices/enableInvoiceHandler'
+import { getInvoiceHandler } from '../../services/invoiceServices/getInvoiceHandler'
+import { updateInvoiceHandler } from '../../services/invoiceServices/updateInvoiceHandler'
+import { listInvoiceHandler } from '../../services/invoiceServices/listinvoiceHandler'
+
+export const createInvoice = async (invoice: InvoiceInterface): Promise<InvoiceInterface> => await createInvoiceHandler(invoice)
+
+export const updateInvoice = async (invoice: InvoiceInterface): Promise<InvoiceInterface> => await updateInvoiceHandler(invoice)
+
+export const disableInvoice = async (invoiceNumber: String): Promise<boolean> => await disableInvoiceHandler(invoiceNumber)
+
+export const enableInvoice = async (invoiceNumber: String): Promise<boolean> => await enableInvoiceHandler(invoiceNumber)
+
+export const getInvoice = async (invoiceNumber: String): Promise<InvoiceInterface | null> => await getInvoiceHandler(invoiceNumber)
+
+export const invoiceList = async (invoiceNumber: String): Promise<InvoiceInterface[] | null> => await listInvoiceHandler()
