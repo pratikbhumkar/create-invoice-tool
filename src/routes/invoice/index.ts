52c175d8af2df078799ddd6c5a1c9335d182c428
pratikbhumkar <prateekbhumkar@gmail.com>
2022-09-20 11:02:09 +1000

    add routes for invoice

--- src/routes/invoice/index.ts
+++ src/routes/invoice/index.ts
@@ -1,25 +1,32 @@
-import router, { Router } from 'express'
+import router, { Request, Response, Router } from 'express'
+import { createInvoice, disableInvoice, getInvoice, invoiceList, updateInvoice } from '../../controllers/invoiceControllers'
+import { InvoiceInterface } from '../../dataInterface/invoiceInterface'
 import { checkJwt } from '../../utils/jwtTokenChecker'
 const invoiceRouter: Router = router.Router()
 
-invoiceRouter.put('/createInvoice', checkJwt, function (req, res, next) {
-  res.send('Welcome')
+invoiceRouter.post('/createInvoice', checkJwt, async function (req: Request<{}, {}, InvoiceInterface>, res: Response, next) {
+  const response = await createInvoice(req.body)
+  res.send(response)
 })
 
-invoiceRouter.get('/getInvoice', checkJwt, function (req, res, next) {
-  res.send('Welcome')
+invoiceRouter.get('/getInvoice', checkJwt, async function (req: Request<{}, {}, string>, res: Response, next) {
+  const response = await getInvoice(req.query.invoiceNumber as string)
+  res.send(response)
 })
 
-invoiceRouter.post('/disableInvoice', checkJwt, function (req, res, next) {
-  res.send('Welcome')
+invoiceRouter.post('/disableInvoice', checkJwt, async function (req: Request<{}, {}, String>, res: Response, next) {
+  const response = await disableInvoice(req.query.invoiceNumber as string)
+  res.send(response)
 })
 
-invoiceRouter.post('/updateInvoice', checkJwt, function (req, res, next) {
-  res.send('Welcome')
+invoiceRouter.post('/updateInvoice', checkJwt, async function (req: Request<{}, {}, InvoiceInterface>, res: Response, next) {
+  const response = await updateInvoice(req.body)
+  res.send(response)
 })
 
-invoiceRouter.get('/invoiceList', checkJwt, function (req, res, next) {
-  res.send('Welcome')
+invoiceRouter.get('/invoiceList', checkJwt, async function (req: Request<{}, {}, String>, res: Response, next) {
+  const response = await invoiceList()
+  res.send(response)
 })
 
 export default invoiceRouter
